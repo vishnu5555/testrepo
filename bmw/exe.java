969133ce1c3c69b084094eba9b1118ef62e74572
Vishnu <vishnu@oracle.com>
2017-07-05 11:56:07 +0530

    java changes

---
+++ bmw/exe.java
@@ -0,0 +1,24 @@
+class PrintStudentDetailsUsingClasses
+{
+    public static void main(String s[])
+    {
+        Student students[] = new Student[5];
+    
+        students[0] = new Student();
+        students[0].name = "Rajesh";
+        students[0].marks = 45;
+        students[0].section = 'A';
+    
+        students[1] = new Student();
+        students[1].name = "Suresh";
+        students[1].marks = 78;
+        students[1].section = 'B';
+    
+        students[2] = new Student();
+        students[2].name = "Ramesh";
+        students[2].marks = 83;
+        students[2].section = 'A';
+    
+        students[3] = new Student();
+        students[3].name = "Kamlesh";
+        students[3].marks = 77;
